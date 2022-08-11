3d2606d5e2d4485ccbf810a87098068bf3ab7c52
Ian <55300372+ClientDNS@users.noreply.github.com>
2022-08-11 04:13:36 +0200

    set config properties

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+git config --global user.email "no-reply@github.com"
+git config --global user.name "TeamCity Actions"
 ./gradlew applyPatches && ./gradlew createReobfPaperclipJar
